25dac723bfa1cfe7de6bafd9e9f82f20d9003aab
Admin <geoffreyngobi17@gmail.com>
2026-05-23 13:18:51 +0300

    Add extend_existing to Expense model to fix table error

--- models.py
+++ models.py
@@ -99,19 +99,10 @@ class Sale(db.Model):
     notes = db.Column(db.Text)
 
     seller = db.relationship('User', backref='sales')
-class Expense(db.Model):
-    __tablename__ = 'expenses'
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    amount = db.Column(db.Numeric(10,2), nullable=False)
-    category = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    recorded_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    user = db.relationship('User', backref='expenses')
 class Expense(db.Model):
     __tablename__ = 'expenses'
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     amount = db.Column(db.Numeric(10,2), nullable=False)
